0a57391ccfd9826e1b1e0158ce984c7f5ccedf9d
lucasptcastro <lucasptcastro@gmail.com>
2023-07-11 09:37:29 -0300

    ✨ feat: added routes

--- main.py
+++ main.py
@@ -3,7 +3,31 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello world teste!"
+    return "Hello world!"
+
+@app.route("/employees/create", methods=["POST"])
+
+# GET ALL
+@app.route("/employees/getall", methods=['GET'])
+def get_employees():
+    return "Colaboradores"
+
+# GET
+@app.route("/employees/get", methods=['GET'])
+def get_employee():
+    return "Colaborador"
+
+# DELETE
+@app.route("/employees/delete", methods=["DELETE"])
+def delete_employee():
+    return "Colaborador excluído!"
+
+# UPDATE
+@app.route("/employees/update", methods=["UPDATE"])
+def update_employee():
+    return "Colaborador atualizado!"
+
+
 
 # Hot reload
 if __name__ == "__main__":
